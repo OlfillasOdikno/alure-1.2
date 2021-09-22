1461b35f4b3f217a098c5299d4971f1434a76019
Your Name <you@example.com>
2021-09-22 04:18:00 +0200

    added docker builds

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 external="$(pwd)/external/win32"
-arch=${1#--}
+
+for i in "$@"; do
+  case $i in
+    --linux* | --win*) arch=${i#--};;
+    --docker-image) docker=true; image=true ci=false;;
+    --docker) docker=true; image=false ci=false;;
+    --ci) docker=true; image=false ci=true;;
+  esac
+done
 
 archs=(
   "linux32"
   "linux64"
   "win32"
   "win64"
+  "linuxarmhf"
 )
 
 hosts=(
@@ -15,6 +24,7 @@ hosts=(
   ""
   "i686-w64-mingw32"
   "x86_64-w64-mingw32"
+  "arm-linux-gnueabihf"
 )
 
 defines=(
@@ -22,13 +32,15 @@ defines=(
   "-DBUILD_SHARED=OFF -DBUILD_EXAMPLES=OFF -DDUMB=OFF -DMODPLUG=OFF"
   "-DBUILD_SHARED=OFF -DBUILD_EXAMPLES=OFF -DDUMB=OFF -DMODPLUG=OFF -DHAS_SNDFILE=1 -DHAS_VORBISFILE=1 -DHAS_FLAC=1 -DHAS_MPG123=1 -DHAS_FLUIDSYNTH=1"
   "-DBUILD_SHARED=OFF -DBUILD_EXAMPLES=OFF -DDUMB=OFF -DMODPLUG=OFF -DHAS_SNDFILE=1 -DHAS_VORBISFILE=1 -DHAS_FLAC=1 -DHAS_MPG123=1 -DHAS_FLUIDSYNTH=1"
+  "-DBUILD_SHARED=OFF -DBUILD_EXAMPLES=OFF -DDUMB=OFF -DMODPLUG=OFF"
 )
 
 ccflags=(
-  "-DCMAKE_CXX_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32"
+  "-DCMAKE_PREFIX_PATH=\"/usr/lib/i386-linux-gnu/\" -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32"
   "-DCMAKE_CXX_FLAGS=-m64 -DCMAKE_C_FLAGS=-m64"
   "-DCMAKE_CXX_FLAGS=\"-I$external/include\" -DCMAKE_TOOLCHAIN_FILE=../XCompile.txt"
   "-DCMAKE_CXX_FLAGS=\"-I$external/include\" -DCMAKE_TOOLCHAIN_FILE=../XCompile.txt"
+  "-DCMAKE_PREFIX_PATH=\"/usr/lib/arm-linux-gnueabihf/\" -DCMAKE_C_COMPILER=arm-linux-gnueabihf-gcc -DCMAKE_CXX_COMPILER=arm-linux-gnueabihf-g++"
 )
 
 output=(
@@ -36,6 +48,7 @@ output=(
   "libalure-static.a"
   "libALURE32-static.a"
   "libALURE32-static.a"
+  "libalure-static.a"
 )
 
 openaldir=(
@@ -43,12 +56,37 @@ openaldir=(
   ""
   "$external"
   "$external"
+  ""
 )
+if [ "$docker" = true ]; then
+  if [ "$image" = true ]; then
+    for i in ${!archs[@]}; do
+      if [ ! -z ${arch} ] && [ ! ${archs[$i]} = ${arch} ]; then
+        continue
+      fi
+      docker build -t alure-builder:${archs[$i]} - < docker/${archs[$i]}.Dockerfile
+    done
+  else
+    for i in ${!archs[@]}; do
+      if [ ! -z ${arch} ] && [ ! ${archs[$i]} = ${arch} ]; then
+        continue
+      fi
+      if [ "$ci" = true ]; then
+        docker run --rm -tv jenkins_home:/var/jenkins_home -w "$(pwd)" debian-rvgl:${archs[$i]} ./build.sh "--${archs[$i]}"
+      else
+        docker run --rm -tv "$(pwd)":/work -w /work alure-builder:${archs[$i]} ./build.sh "--${archs[$i]}"
+      fi
+    done
+  fi
 
-for i in ${!archs[@]}; do
+  exit
+fi
+
+for i in ${!archs[@]}; do  
   if [ ! -z ${arch} ] && [ ! ${archs[$i]} = ${arch} ]; then
     continue
   fi
+
   mkdir -p "build_${archs[$i]}"
   export OPENALDIR=${openaldir[$i]}
   prefix=`[ ! -z ${hosts[$i]} ] && echo ${hosts[$i]}-`
